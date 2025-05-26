2a8d9c078be31a4bf5821f0de3f8ef18b295eac8
EnzoAlzaga <enzoalzaga@hotmail.com>
2025-05-26 15:57:53 -0300

    Add files via upload

---
+++ atividade-módulos-pacotes.py
@@ -0,0 +1,27 @@
+import statistics
+n = [2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22, 24, 26, 28]
+x = statistics.mean(n)
+z = statistics.median(n)
+print(x)
+print(z)
+
+import statistics as est
+n = [2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22, 24, 26, 28]
+x = est.mean(n)
+z = est.median(n)
+print(x)
+print(z)
+
+from statistics import mean, median 
+n = [2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22, 24, 26, 28]
+x = mean(n)
+z = median(n)
+print(x)
+print(z)
+
+from statistics import *
+n = [2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22, 24, 26, 28]
+x = mean(n)
+z = median(n)
+print(x)
+print(z)
